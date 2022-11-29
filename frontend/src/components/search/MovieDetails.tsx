4ac052e7fe4c68c43236e594e3303f3cd6466d85
adomaitisc <caadomaitis@gmail.com>
2022-11-29 02:05:22 -0500

    added functionality to collect user rating

--- frontend/src/components/search/MovieDetails.tsx
+++ frontend/src/components/search/MovieDetails.tsx
@@ -1,5 +1,6 @@
 import { MovieType } from ".";
 import { FaAngleLeft, FaCheck } from "react-icons/fa";
+import { useState } from "react";
 
 export const MovieDetails = ({
   movie,
@@ -10,41 +11,59 @@ export const MovieDetails = ({
   handleClose: () => void;
   handleSelect: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }) => {
+  const [selected, setSelected] = useState("");
   return (
     <div className="fixed top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-black/50 backdrop-blur-md bg-opacity-50 z-10">
       <main className="w-3/5  flex flex-col items-center gap-y-2">
         <img className="rounded-lg w-1/3 mb-4" src={movie.i.imageUrl} />
         <p className="text-white text-3xl font-medium">{movie.l}</p>
-        <p className="text-zinc-400 text-lg">{movie.s}</p>
+        <p className="text-zinc-400 text-lg">
+          Select your thoughts on the movie
+        </p>
         <div className="flex flex-col gap-y-2 justify-center items-center">
           <div className="flex gap-x-2">
             <button
               id="1"
-              className="px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500"
+              onClick={(e) => setSelected(e.currentTarget.id)}
+              className={`${
+                selected === "1" && `bg-violet-800`
+              }   px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500`}
             >
               Awful
             </button>
             <button
               id="2"
-              className="px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500"
+              onClick={(e) => setSelected(e.currentTarget.id)}
+              className={`${
+                selected === "2" && `bg-violet-800`
+              }   px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500`}
             >
               Bad
             </button>
             <button
               id="3"
-              className="px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500"
+              onClick={(e) => setSelected(e.currentTarget.id)}
+              className={`${
+                selected === "3" && `bg-violet-800`
+              }   px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500`}
             >
               Mid
             </button>
             <button
               id="4"
-              className="px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500"
+              onClick={(e) => setSelected(e.currentTarget.id)}
+              className={`${
+                selected === "4" && `bg-violet-800`
+              }   px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500`}
             >
               Good
             </button>
             <button
               id="5"
-              className="px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500"
+              onClick={(e) => setSelected(e.currentTarget.id)}
+              className={`${
+                selected === "5" && `bg-violet-800`
+              }   px-2 py-2 border-2 border-zinc-300 text-zinc-300 rounded-md hover:text-violet-500 hover:border-violet-500`}
             >
               Great
             </button>
@@ -61,7 +80,8 @@ export const MovieDetails = ({
           <button
             onClick={(e) => handleSelect(e)}
             id={movie.id}
-            className="text-orange-400 hover:text-orange-800 flex items-center gap-x-2 rounded-md text-lg font-medium duration-300"
+            disabled={selected === ""}
+            className=" disabled:text-zinc-600 text-orange-400 hover:text-orange-800 flex items-center gap-x-2 rounded-md text-lg font-medium duration-300"
           >
             <FaCheck />
             Add to Collection
